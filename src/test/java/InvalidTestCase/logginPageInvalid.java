1f22e990c5ede5c93ebc90f04c2d6351ac378027
Ahmedshaban <ahmedshaban744@gmail.com>
2026-03-21 23:29:56 +0200

    validate error massage close

--- src/test/java/InvalidTestCase/logginPageInvalid.java
+++ src/test/java/InvalidTestCase/logginPageInvalid.java
@@ -5,13 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import java.time.Duration;
 
-public class logginPage {
+public class logginPageInvalid {
     @DataProvider(name = "loginInvalidData")
     public Object[][] getData() {
         return new Object[][] {
@@ -30,19 +29,34 @@ public class logginPage {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
+
         WebElement usernames = driver.findElement(By.xpath("//input[@placeholder=\"Username\"]"));
         usernames.sendKeys(username);
+
         WebElement passwords = driver.findElement(By.xpath("//input[@placeholder=\"Password\"]"));
         passwords.sendKeys(password);
+
         WebElement Button = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
         Button.click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        String Expectet= driver.getCurrentUrl();
-        wait.until(ExpectedConditions.urlToBe("https://www.saucedemo.com/"));
-        String Actual ="https://www.saucedemo.com/";
-        Assert.assertEquals(Actual,Expectet);
-        driver.quit();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+// wait for visibility
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[@data-test='error']")));
+
+        WebElement massage = driver.findElement(By.xpath("//h3[@data-test='error']"));
+        Assert.assertTrue(massage.isDisplayed());
+
+        WebElement Closs = driver.findElement(By.xpath("//button[@class='error-button']"));
+        Closs.click();
+
+// wait for invisibility after clicking close
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//h3[@data-test='error']")));
+
+//  re-check using findElements instead of stale WebElement
+        Assert.assertTrue(driver.findElements(By.xpath("//h3[@data-test='error']")).isEmpty());
+
+        driver.quit();
     }
 
 }
